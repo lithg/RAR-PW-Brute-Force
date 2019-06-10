63cf77982ffabe454e15f4f91512749a06e04952
Guilherme Carvalho Lithg <35541154+lithg@users.noreply.github.com>
2019-06-10 16:12:21 -0300

    Password generator

---
+++ gerador.py
@@ -0,0 +1,6 @@
+from itertools import chain, product
+
+def gerador(charset, maxlength):
+    return (''.join(candidate)
+        for candidate in chain.from_iterable(product(charset, repeat=i)
+        for i in range(1, maxlength + 1)))
